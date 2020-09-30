5c856ca6b54f1115e6c3d3748729622f02565b5e
Aliona <Aliona@DESKTOP-8PRTMSV>
2020-09-30 19:56:27 +0300

    30.09.2020

---
+++ src/lt/vtmc/aliona/pd9/PD_3.java
@@ -0,0 +1,17 @@
+package lt.vtmc.aliona.pd9;
+
+public class PD_3 {
+
+	public static void main(String[] args) {
+		int [] numbers = {23,45,12,3,78,98,15,-3};
+		
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i]%2 == 0) {
+	System.out.println(numbers[i]);
+	
+}
+
+	}
+
+	}
+}
